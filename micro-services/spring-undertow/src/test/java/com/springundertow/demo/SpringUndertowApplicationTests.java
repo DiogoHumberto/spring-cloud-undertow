569372f917bf0fb8b72180dbf55793db1bea9c1c
Diogo Humberto <diogo_humberto@hotmail.com>
2022-07-19 17:36:39 -0300

    inciando com undertow

---
+++ micro-services/spring-undertow/src/test/java/com/springundertow/demo/SpringUndertowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springundertow.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUndertowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
